ab87d8019b13aa81745ab1fe677cd333ddc7a7c2
Nirel Chua <95299412+cninz@users.noreply.github.com>
2022-01-15 13:10:19 -0700

    Add files via upload

--- casting.py
+++ casting.py
@@ -1,74 +1,15 @@
-"""
-int
-float
-complex
-"""
+# Integers
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
 
-x = 1    # int
-y = 2.8  # float
-z = 1j   # complex
+# Float
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
 
-print(type(x))
-print(type(y))
-print(type(z))
-
-# int
-x = 1
-y = 35656222554887711
-z = -3255522
-
-print(type(x))
-print(type(y))
-print(type(z))
-
-# float
-x = 1.10
-y = 1.0
-z = -35.59
-
-print(type(x))
-print(type(y))
-print(type(z))
-
-x = 35e3
-y = 12E4
-z = -87.7e100
-
-print(type(x))
-print(type(y))
-print(type(z))
-
-# complex
-x = 3+5j
-y = 5j
-z = -5j
-
-print(type(x))
-print(type(y))
-print(type(z))
-
-# type conversion
-x = 1   # int
-y = 2.8 # float
-z = 1j  # complex
-
-# convert from int to float:
-a = float(x)
-
-# convert from float to int:
-b = int(y)
-
-# convert from into to complex:
-c = complex (x)
-
-print(a))
-print(b))
-print(c))
-
-print(type(a))
-print(type(b))
-print(type(c))
-
-# Random Number
-import random
-print (random, randrange(1, 10))
+# Strings
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
